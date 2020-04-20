7e24a8cb311f26d4294e9c2caf93f6dacefce88c
Graham Small <graham_small@live.co.uk>
2020-04-20 16:39:32 +0100

    Fix instructions to create (update) streetLighting.db

--- StreetLightingDal/Data/StreetLightingDBContext.cs
+++ StreetLightingDal/Data/StreetLightingDBContext.cs
@@ -14,9 +14,9 @@ namespace StreetLightingDal.Data
     }
 }
 
-//(to run migrations in console):
+//(to run migrations in console from startup project - StreetLighting):
 //dotnet ef migrations add initialCreate --output-dir Data/Migrations
-//dotnet ef database update
+//dotnet ef database update -p ..\StreetLightingDal\StreetLightingDal.csproj -s .\StreetLighting.csproj
 //to remove:
 //dotnet ef database update initial (or 'dotnet ef database update 0' to remove all)
 //and
